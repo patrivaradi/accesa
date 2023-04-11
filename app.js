d9497776bf8ef7a570161a2270f88a8bdd4a2d26
Patri <Patri@DESKTOP-KBKHMBK>
2023-04-11 22:09:22 +0300

    minor changes

--- app.js
+++ app.js
@@ -12,16 +12,18 @@ const form = document.getElementById('location_input');
 const search = document.querySelector('.search');
 const submit = document.querySelector('.submit');
 
+let cityInput = 'Oradea';
 
-const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 const APIkey = '111dd2e8dc614eaaa84143103230504';
-// Showing the right date and time
+
+// Updating current date and time
 setInterval(() => {
     const time = new Date();
     const month = time.getMonth();
-    
     const date = time.getDate();
     const day = time.getDay();
     const hour = time.getHours();
@@ -30,53 +32,54 @@ setInterval(() => {
     const ampm = hour >=12 ? 'PM': 'AM';
     
     currentTime.innerHTML = `${militaryhours}:${minutes} <span id="am-pm">${ampm}</span>`;
-    
     currentDate.innerHTML = `${days[day]}, ${date}  ${months[month]}`
+
+    if(hour < 8 || hour > 19)
+    {
+        document.getElementById('body').style.backgroundImage='url("https://images.unsplash.com/photo-1632446628687-127cf068a522?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80")'
+    }
 },1000);
-//--------------------------------------------------
+
+
 //Adding submit event to the <form>
 form.addEventListener('submit', (e) =>{
     if(search.value.length == 0){
         alert('Please type in a city name');
     }
     else{
-        city
+        cityInput = search.value;
+        WeatherData();
+        search.value='';
     }
+    e.preventDefault();
 })
-
+WeatherData();
 // Getting data from the weather API
 function WeatherData () {
-    navigator.geolocation.getCurrentPosition((succes)=>{
-        console.log(succes);
-        let {latitude, longitude} = succes.coords;
-        fetch(`http://api.weatherapi.com/v1/forecast.json?key=${APIkey}&days=7&aqi=no&alerts=no&q=${latitude},${longitude}`).then(res=>res.json()).then(data =>{
-            console.log(data)
-            currentTemp.innerHTML = data.current.temp_c + '&#176;' + ' C';
-            currentDesc.innerHTML = data.current.condition.text;
-            currentWeatherIcon.src = data.current.condition.icon;
-            currentFeelsLike.innerHTML = data.current.feelslike_c + '&#176;' + ' C';
-            currentHumidity.innerHTML = data.current.humidity +  ' %';
-            currentWindSpeed.innerHTML = data.current.wind_kph + ' km/h';
-            
-            
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=${APIkey}&days=7&aqi=no&alerts=no&q=${cityInput}`).then(res=>res.json()).then(data =>{
+        console.log(data)
 
-            forecast.innerHTML=''
-            data.forecast.forecastday.forEach((day,idx)=>
-                {//console.log(day)
-                    const epochDate=new Date(data.forecast.forecastday[idx].date_epoch *1000);
-                    forecast.innerHTML += 
-                    `
-                    <div class="weather-forecast">
-                        <div class="day">${days[epochDate.getDay()]}</div>
-                        <img src="${data.forecast.forecastday[idx].day.condition.icon}" alt="Weather icon" class="forecast_icon">
-                        <div class="temp_day">${data.forecast.forecastday[idx].day.maxtemp_c}&#176; C</div>
-                        <div class="temp_night">${data.forecast.forecastday[idx].day.mintemp_c}&#176; C</div>
-                    </div>
-                    `
-                })
-            
+        //Getting current weather data from API and changing it in html
+        currentCity.innerHTML = data.location.name;
+        currentTemp.innerHTML = data.current.temp_c + '&#176;' + ' C';
+        currentDesc.innerHTML = data.current.condition.text;
+        currentWeatherIcon.src = data.current.condition.icon;
+        currentFeelsLike.innerHTML = data.current.feelslike_c + '&#176;' + ' C';
+        currentHumidity.innerHTML = data.current.humidity +  ' %';
+        currentWindSpeed.innerHTML = data.current.wind_kph + ' km/h';
 
+        //getting forecast data from API 
+        forecast.innerHTML=''
+        data.forecast.forecastday.forEach((day,idx)=>
+        {
+            const epochDate=new Date(day.date_epoch *1000);
+            forecast.innerHTML += 
+            `<div class="weather-forecast">
+                <div class="day">${days[epochDate.getDay()]}</div>
+                <img src="${data.forecast.forecastday[idx].day.condition.icon}" alt="Weather icon" class="forecast_icon">
+                <div class="temp_day">${data.forecast.forecastday[idx].day.maxtemp_c}&#176; C</div>
+                <div class="temp_night">${data.forecast.forecastday[idx].day.mintemp_c}&#176; C</div>
+            </div>`
         })
     })
 };
-WeatherData();
